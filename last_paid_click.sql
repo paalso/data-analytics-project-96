acb77d5683d21a21d4800880038f3929ff1f7ef9
Pavel Soroka <40a.pvl@gmail.com>
2024-12-15 17:39:23 +0200

    Fix Last Paid Click data mart and last_paid_click.csv

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,19 +1,35 @@
+WITH ranked_sessions AS (
+    SELECT
+        s.visitor_id,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        s.visit_date,
+        ROW_NUMBER() OVER (
+            PARTITION BY l.lead_id
+            ORDER BY s.visit_date DESC
+        ) AS row_num
+    FROM sessions AS s
+    LEFT JOIN leads AS l
+        ON s.visitor_id = l.visitor_id
+    WHERE s.medium != 'organic'
+)
 SELECT
-    s.visitor_id,
-    s.visit_date,
-    s.source AS utm_source,
-    s.medium AS utm_medium,
-    s.campaign AS utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-FROM sessions AS s
-LEFT JOIN leads AS l
-    ON s.visitor_id = l.visitor_id
-order BY
-    amount DESC NULLS LAST,
-    visit_date,
-    utm_source, utm_medium, utm_campaign
-LIMIT 10;
+    visitor_id,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id,
+    visit_date
+FROM ranked_sessions
+WHERE row_num = 1
+ORDER BY amount DESC NULLS LAST, visit_date, utm_source, utm_medium, utm_campaign;
